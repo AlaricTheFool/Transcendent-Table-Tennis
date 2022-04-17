aa5d53f3e86154a5fec3bf30861b3459d0456d0a
Alaric <alaric@arcanazero.com>
2022-04-16 22:08:02 -0600

    Render ball and paddles

---
+++ README.md
@@ -0,0 +1,8 @@
+# Transcendental Table Tennis
+
+It's literally just pong.
+
+## Plan
+
+1. Set up ECS basics
+2. Make Pong
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,86 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("BasicShapes")]
-async fn main() {
-    loop {
-        clear_background(RED);
+const BG_COLOR:     Color = Color::new(031. / 255., 039. / 255., 027. / 255., 1.0);
+const PADDLE_COLOR: Color = Color::new(052. / 255., 127. / 255., 196. / 255., 1.0);
+const BALL_COLOR:   Color = Color::new(204. / 255., 041. / 255., 054. / 255., 1.0);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-        draw_text("HELLO", 20.0, 20.0, 20.0, DARKGRAY);
+const BALL_RADIUS: f32 = 16.;
+
+const WINDOW_WIDTH: i32 = 1280;
+const WINDOW_HEIGHT: i32 = 720;
+
+struct Ball {
+    pos: Vec2,
+    velocity: Vec2,
+}
+
+impl Ball {
+    fn new() -> Self {
+        Self {
+            pos: center_screen(),
+            velocity: Vec2::ZERO,
+        }
+    }
+}
+
+fn center_screen() -> Vec2 {
+    Vec2::new(screen_width() / 2.0, screen_height() / 2.0)
+}
+
+struct PongMatch {
+    ball: Ball,
+    left_paddle_pos: Vec2,
+    right_paddle_pos: Vec2,
+}
+
+const PADDLE_WINDOW_EDGE_OFFSET: f32 = 16.;
+
+impl PongMatch {
+    fn new() -> Self {
+        Self {
+            ball: Ball::new(),
+            left_paddle_pos: Vec2::new(PADDLE_WINDOW_EDGE_OFFSET + (PADDLE_WIDTH * 0.5), screen_height() / 2.0),
+            right_paddle_pos: Vec2::new(screen_width() - (PADDLE_WINDOW_EDGE_OFFSET + (PADDLE_WIDTH * 0.5)), screen_height() / 2.0),
+        }
+    }
+
+    fn render(&self) {
+        clear_background(BG_COLOR);
+
+        draw_circle(self.ball.pos.x, self.ball.pos.y, BALL_RADIUS, BALL_COLOR);
+
+        draw_paddle(self.left_paddle_pos);
+        draw_paddle(self.right_paddle_pos);
+    }
+}
+
+const PADDLE_WIDTH: f32 = 16.;
+const PADDLE_HEIGHT: f32 = 128.;
+
+fn draw_paddle(pos: Vec2) {
+    draw_rectangle(
+        pos.x - (PADDLE_WIDTH * 0.5), 
+        pos.y - (PADDLE_HEIGHT * 0.5), 
+        PADDLE_WIDTH, 
+        PADDLE_HEIGHT, 
+        PADDLE_COLOR)
+}
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Transcendent Table Tennis".to_owned(),
+        window_width: WINDOW_WIDTH,
+        window_height: WINDOW_HEIGHT,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    let mut pong_match = PongMatch::new();
+
+    loop { 
+        pong_match.render();
 
         next_frame().await
     }
